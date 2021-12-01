8c7815b330233bb3de3626b84cf6156b280450f1
Nathan Watkins <nathancwatkins23@gmail.com>
2021-12-01 14:07:07 -0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['airbnb', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'no-console': 'off',
+        'no-use-before-define': 'off',
+        'no-underscore-dangle': 'off',
+        'no-param-reassign': 'off',
+        'consistent-return': 'off',
+    },
+    plugins: ['prettier'],
+}
