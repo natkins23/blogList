35f9dfdc0d573c3f1213ebd1573ca3c8079ec2d1
Nathan Watkins <nathancwatkins23@gmail.com>
2021-12-15 10:40:20 -0800

    Exercise 4.5 completed:

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,21 @@ const totalLikes = blogList => {
     return blogList.map(blogs => blogs.likes).reduce(reducer, 0)
 }
 
+const findFavoriteBlog = blogList => {
+    const arrayOfLikes = blogList.map(blog => blog.likes)
+    if (arrayOfLikes.length === 0) {
+        return 'no favorite blog'
+    }
+    const largestLike = Math.max(...arrayOfLikes)
+    const favoriteBlog = blogList.find(blog => blog.likes === largestLike)
+    delete favoriteBlog._id
+    delete favoriteBlog.__v
+    delete favoriteBlog.url
+    return favoriteBlog
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    findFavoriteBlog,
 }
