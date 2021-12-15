d58f1e18d7290441fd2dfcd6c1215c35edff4235
Nathan Watkins <nathancwatkins23@gmail.com>
2021-12-15 10:08:34 -0800

    Exercise 4.4 completed:

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = () => 1
 
+const totalLikes = blogList => {
+    const reducer = (previousValue, currentValue) => previousValue + currentValue
+    return blogList.map(blogs => blogs.likes).reduce(reducer, 0)
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
