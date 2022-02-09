9a0d43d62440bfe977335ead4ca27facd07b49ac
Nathan Watkins <nathancwatkins23@gmail.com>
2022-02-09 15:25:40 -0800

    Exercise 4.18 completed:

---
+++ controllers/login.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router() //
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+loginRouter.post('/', async (req, res) => {
+    const { body } = req
+    const user = await User.findOne({ username: body.username })
+
+    const passwordCorrect = user === null ? false : bcrypt.compare(body.password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+        return res.status(400).json({ error: 'invalid username or password' })
+    }
+    const userForToken = { username: user.username, id: user._id }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    res.status(200).send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
